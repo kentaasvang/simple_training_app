66297ce7f5b6e39b43249bcff85917a36d207866
Kent Martin Åsvang <github.com@asvang.no>
2024-10-19 19:31:04 +0200

    init db schema

---
+++ data/0001-schema.sql
@@ -0,0 +1,53 @@
+
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL
+);
+
+
+CREATE TABLE exercises (
+    exercise_id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT
+);
+
+
+CREATE TABLE workouts (
+    workout_id TEXT PRIMARY KEY NOT NULL,
+    user_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE exercises_to_workouts (
+    exercise_to_workout_id TEXT PRIMARY KEY NOT NULL,
+    exercise_id TEXT NOT NULL,
+    workout_id TEXT NOT NULL,
+
+    FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id),
+    FOREIGN KEY (workout_id) REFERENCES workouts(workout_id)
+);
+
+
+CREATE TABLE exercise_details (
+    exercise_detail_id TEXT PRIMARY KEY NOT NULL,
+    reps INTEGER NOT NULL,
+    sets INTEGER NOT NULL,
+    weight REAL NOT NULL,
+    exercise_to_workout_id TEXT NOT NULL,
+
+    FOREIGN KEY (exercise_to_workout_id) REFERENCES exercises_to_workouts(exercise_to_workout_id)
+);
+
+
+CREATE TABLE sessions (
+    session_id TEXT PRIMARY KEY NOT NULL,
+    date TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    workout_id TEXT NOT NULL,
+
+    FOREIGN KEY (workout_id) REFERENCES workouts(workout_id)
+);
+
---
+++ data/0002-insert_users.sql
@@ -0,0 +1,7 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO users (user_id, username) 
+VALUES ("6b21e737-d9c5-4521-bedf-285109e9ebf5", "Kent");
+
+INSERT INTO users (user_id, username) 
+VALUES ("8bb62bf8-397a-4644-a8a7-377caf6df30d", "Marte");
