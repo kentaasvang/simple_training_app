141bc0f87c0fd1a43f8033f6767c97b57f8b8ba9
Kent Martin Åsvang <github.com@asvang.no>
2024-10-20 12:50:24 +0200

    installed dotenv and moved BASE_URL to env-file

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
+from os import getenv
 from flask import Flask, render_template_string
+from dotenv import load_dotenv
 
+load_dotenv(".env.dev")
+STA_BASE_URL = getenv("STA_BASE_URL", "/")
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route(STA_BASE_URL)
 def index():
     return render_template_string("<h1> hello world")
